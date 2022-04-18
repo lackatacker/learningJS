a8cc21bc6001c331f7fbc4e6e5b33feb3eb8524c
lackatacker <80588946+lackatacker@users.noreply.github.com>
2022-04-18 13:35:58 +0000

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -30,3 +30,28 @@ document.getElementById('code').addEventListener('input', function(e){
 	}
 }
 )
+function send(e) {
+  e.preventDefault();
+  fetch("https://mockbin.com/request", {
+    method: "POST",
+    headers: {
+      'Accept': 'application/json', 
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(document.getElementById("value").value)
+  })
+  .then(function(res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(function(value) {
+      document
+        .getElementById("result2")
+        .innerText = value.postData.text;
+  });
+}
+
+document
+  .getElementById("form")
+  .addEventListener("submit", send);
